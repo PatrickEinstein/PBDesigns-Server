72cb94262ddc71026c8f48507091e9f5857f79ed
PatrickEinstein <mohammedola1234@gmail.com>
2025-05-26 10:08:53 +0100

    add cron

--- dist/config/KeepAwake.js
+++ dist/config/KeepAwake.js
@@ -1,8 +1,15 @@
 import cron from "node-cron";
 export const KeepAlive = () => {
     try {
-        cron.schedule("* */10 * * *", () => {
+        // cron.schedule("* */10 * * *", async () => {
+        cron.schedule("*/10 * * * *", async () => {
             console.log("I AM WAKE FROM CRON");
+            const url = "https://www.google.com/";
+            const fetched = await fetch(url, {
+                method: "GET",
+            });
+            const res = await fetched.text();
+            console.log(res);
         });
     }
     catch (e) {
--- src/config/KeepAwake.ts
+++ src/config/KeepAwake.ts
@@ -2,10 +2,19 @@ import cron from "node-cron";
 
 export const KeepAlive = () => {
   try {
-    cron.schedule("* */10 * * *", () => {
+    // cron.schedule("* */10 * * *", async () => {
+    cron.schedule("*/10 * * * *", async () => {
       console.log("I AM WAKE FROM CRON");
+
+      const url = "https://www.google.com/";
+      const fetched = await fetch(url,{
+        method: "GET",
+      });
+      const res = await fetched.text();
+      console.log(res)
+     
     });
   } catch (e: any) {
-    console.log(`Cron error==>`, e.message)
+    console.log(`Cron error==>`, e.message);
   }
 };
